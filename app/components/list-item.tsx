46990b143a27b08add55ec282527bf21cd4e34b5
Zainal21 <Zainalarifin080718@gmail.com>
2022-06-28 16:26:51 +0700

    fix : style hover list item

--- app/components/list-item.tsx
+++ app/components/list-item.tsx
@@ -6,7 +6,7 @@ export interface ListItemProps {
 export const ListItem: React.FC<ListItemProps> = (props) => {
   return (
     <div className="container flex justify-center  mx-auto my-2">
-      <ul className="bg-white shadow-md rounded-sm border border-gray-200 w-screen text-gray-900 shadow hover:shadow-xl mx-2">
+      <ul className="bg-white shadow-md rounded-sm border border-gray-200 w-screen text-gray-900 shadow hover:shadow-md mx-2">
         <li className="px-8 py-2 max-w-full rounded-b-lg">
           <p className="font-bold text-lg">{props.jokes}</p>
           <p className="text-md">{props.content}</p>
