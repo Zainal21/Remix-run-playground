d69d8c8258830d1ada4c56a929c09b66aa29b043
Zainal21 <Zainalarifin080718@gmail.com>
2022-06-29 09:20:05 +0700

    remove panel view notification

--- app/components/headers-page.tsx
+++ app/components/headers-page.tsx
@@ -48,7 +48,6 @@ export default function HeadersPage() {
                     type="button"
                     className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                   >
-                    <span className="sr-only">View notifications</span>
                     {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
                   </button>
                 </div>
